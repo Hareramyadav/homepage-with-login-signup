b32a9c4930fc09ff9b61dc85ac85bc62e01d974c
Hareram yadav <yhareram73@gmail.com>
2020-05-25 12:53:49 +0545

    Updates

---
+++ precache-manifest.e9e5caba9ba78cd47a31b23dfa4a37d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76af9560ca11cc1ff26544129028cd81",
+    "url": "/sample-react/index.html"
+  },
+  {
+    "revision": "0044d5bf43d0963b0a6d",
+    "url": "/sample-react/static/css/main.c1251f90.chunk.css"
+  },
+  {
+    "revision": "491c3e8e7e027d124cf2",
+    "url": "/sample-react/static/js/2.bd9f2082.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sample-react/static/js/2.bd9f2082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0044d5bf43d0963b0a6d",
+    "url": "/sample-react/static/js/main.24f90f4e.chunk.js"
+  },
+  {
+    "revision": "4cc0a32aa3ede97ce5d1",
+    "url": "/sample-react/static/js/runtime-main.a43697e8.js"
+  },
+  {
+    "revision": "3dc026861602d93cde60704192d65080",
+    "url": "/sample-react/static/media/homebanner.3dc02686.jpg"
+  },
+  {
+    "revision": "79474cd2351b0c721b1f46a27d000dbb",
+    "url": "/sample-react/static/media/motion-logo.79474cd2.jpg"
+  }
+]);
